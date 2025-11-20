5838da19ad5cdba0ed33763cd05cefe73c23abe6
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-11-20 11:47:41 +0000

    add external button

--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
 /**
  * Connect resistor between -ve pin of LED & GND
  */
-input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P16, 0)
-    basic.showIcon(IconNames.No)
-})
-input.onButtonPressed(Button.B, function () {
+input.onPinPressed(TouchPin.P0, function () {
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.showIcon(IconNames.Yes)
 })
-basic.showIcon(IconNames.Square)
+input.onPinReleased(TouchPin.P0, function () {
+    pins.digitalWritePin(DigitalPin.P16, 0)
+    basic.showIcon(IconNames.No)
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Triangle)
 basic.forever(function () {
 	
 })
