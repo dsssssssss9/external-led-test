55f903935abc5886e9480ece34f52e5952d7ad1d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2025-11-20 13:23:25 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 /**
- * Connect resistor between -ve pin of LED & GND
+ * Connect 1 corner of switch to PIN0
+ * 
+ * Connect opposite corner of switch to GND
+ * 
+ * Connect Anode ( long leg ) of LED to PIN16
+ * 
+ * Connect Cathode ( short Leg ) of LED to 1 leg  of the Resistor
+ * 
+ * Connect other leg resistor to  GND
  */
 input.onPinPressed(TouchPin.P0, function () {
     pins.digitalWritePin(DigitalPin.P16, 1)
@@ -9,14 +17,5 @@ input.onPinReleased(TouchPin.P0, function () {
     pins.digitalWritePin(DigitalPin.P16, 0)
     basic.showIcon(IconNames.No)
 })
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.B, function () {
-	
-})
 pins.digitalWritePin(DigitalPin.P16, 0)
 basic.showIcon(IconNames.Triangle)
-basic.forever(function () {
-	
-})
